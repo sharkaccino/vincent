e48c267534601ac525133cfcd897cc81d18a9fd6
JackDotJS <jackdotjs@proton.me>
2025-07-24 18:55:36 -0700

    canvas styling
    Signed-off-by: JackDotJS <jackdotjs@proton.me>

--- scripts/ui/editor_viewport.gd
+++ scripts/ui/editor_viewport.gd
@@ -1,25 +1,30 @@
 extends ScrollContainer
 
-@onready var margin = $CanvasMargin
+@onready var margin = get_node("%CanvasMargin")
+
+var fix_left = 12
+var fix_right = 18
+var fix_top = 12
+var fix_bottom = 18
 
 func center_scroll_bars() -> void:
 	await get_tree().process_frame
-	var innerSize = $CanvasMargin.get_size()
+	var innerSize = margin.get_size()
 	var containerSize = get_size()
 	var maxX = max(0, innerSize.x - containerSize.x)
 	var maxY = max(0, innerSize.y - containerSize.y)
 	
-	scroll_horizontal = floor(maxX / 2)
-	scroll_vertical = floor(maxY / 2)
+	scroll_horizontal = floor((maxX / 2) + fix_left) 
+	scroll_vertical = floor((maxY / 2) + fix_top)
 
 func _on_resize() -> void:
 	await get_tree().process_frame
 	
 	var newDimensions = get_size()
-	margin.add_theme_constant_override("margin_left", newDimensions.x / 2)
-	margin.add_theme_constant_override("margin_right", newDimensions.x / 2)
-	margin.add_theme_constant_override("margin_top", newDimensions.y / 2)
-	margin.add_theme_constant_override("margin_bottom", newDimensions.y / 2)
+	margin.add_theme_constant_override("margin_left", (newDimensions.x / 2) - fix_left)
+	margin.add_theme_constant_override("margin_right", (newDimensions.x / 2) - fix_right)
+	margin.add_theme_constant_override("margin_top", (newDimensions.y / 2) - fix_top)
+	margin.add_theme_constant_override("margin_bottom", (newDimensions.y / 2) - fix_bottom)
 	
 	center_scroll_bars()
 	
