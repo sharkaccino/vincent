1455bbd8341d1a6061a2dc87f557490473a46bac
JackDotJS <jackdotjs@proton.me>
2025-07-27 08:13:45 -0700

    disable swap orientation button when ratio is 1:1
    Signed-off-by: JackDotJS <jackdotjs@proton.me>

--- scripts/ui/new_image/swap_orientation_button.gd
+++ scripts/ui/new_image/swap_orientation_button.gd
@@ -4,7 +4,14 @@ extends Button
 @onready var width_input = get_node("%WidthInput")
 @onready var height_input = get_node("%HeightInput")
 
-func _on_pressed() -> void:
+func check_should_disable(_a) -> void:
+	# no point in giving the user this option when it would effectively do nothing
+	if width_input.value == height_input.value:
+		disabled = true
+	else:
+		disabled = false
+
+func on_pressed() -> void:
 	var new_width = height_input.value
 	var new_height = width_input.value
 	var new_ratio = 1 / keep_ratio_checkbox.get_meta("target_ratio")
@@ -14,4 +21,6 @@ func _on_pressed() -> void:
 	height_input.value = new_height
 	
 func _ready() -> void:
-	pressed.connect(_on_pressed)
+	pressed.connect(on_pressed)
+	width_input.value_changed.connect(check_should_disable)
+	height_input.value_changed.connect(check_should_disable)
