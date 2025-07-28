b031edd6ece246b494426339fb869f1d4126637e
JackDotJS <jackdotjs@proton.me>
2025-07-28 01:48:01 -0700

    show full filename in tab tooltips
    Signed-off-by: JackDotJS <jackdotjs@proton.me>

--- scripts/ui/project_tab.gd
+++ scripts/ui/project_tab.gd
@@ -1,8 +1,12 @@
 extends Button
 
+@onready var project_id = get_meta("project_id")
+
 func _on_pressed() -> void:
 	# TODO: ignore if this is already the active project
-	StateManager.set_active_project(get_meta("project_id"))
+	StateManager.set_active_project(project_id)
 
 func _ready() -> void:
+	var this_project = StateManager.get_project_data(project_id)
+	tooltip_text = this_project.name
 	pressed.connect(_on_pressed)
