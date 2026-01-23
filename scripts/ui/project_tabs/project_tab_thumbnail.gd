373b2505e087a2b549eb42a5a9c3334d532af7bf
Jackie <sharkaccino@fastmail.com>
2026-01-22 20:11:14 -0800

    project tabs: toggle texture filtering based on image size
    Signed-off-by: Jackie <sharkaccino@fastmail.com>

--- scripts/ui/project_tabs/project_tab_thumbnail.gd
+++ scripts/ui/project_tabs/project_tab_thumbnail.gd
@@ -5,6 +5,19 @@ extends TextureRect
 # TODO: show the combined output of all layers merged together.
 
 func _ready() -> void:
-	var current_project = StateManager.get_project_data(get_node("../../../..").get_meta("project_id"))
-	if current_project.id == 0: return
-	texture = ImageTexture.create_from_image(current_project.layers[0].image_data)
+	var connected_project = StateManager.get_project_data(get_node("../../../..").get_meta("project_id"))
+	if connected_project.id == 0: return
+	
+	texture = ImageTexture.create_from_image(connected_project.layers[0].image_data)
+	
+	await get_tree().process_frame
+	
+	var t_size = get_rect().size
+	var p_size = connected_project.size
+	
+	print(t_size, p_size, p_size.x > t_size.x || p_size.y > t_size.y)
+	
+	if (p_size.x > t_size.x || p_size.y > t_size.y):
+		texture_filter = CanvasItem.TEXTURE_FILTER_LINEAR_WITH_MIPMAPS
+	else:
+		texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
