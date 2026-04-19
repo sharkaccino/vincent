6e99c48394655d971e70d19ac34515a4ec265040
Jackie <sharkaccino@fastmail.com>
2026-04-19 06:49:06 -0700

    update app icon, add about dialog
    Signed-off-by: Jackie <sharkaccino@fastmail.com>

--- scripts/ui/menu_bar/help_menu_button.gd
+++ scripts/ui/menu_bar/help_menu_button.gd
@@ -1,8 +1,16 @@
 extends MenuButton
 
+var about_window
+
 func _on_id_pressed(id) -> void:
 	if (id == 0): OS.shell_open("https://github.com/sharkaccino/vincent/issues")
+	if (id == 1): about_window.popup()
 
 func _ready() -> void:
 	var popup = get_popup()
 	popup.id_pressed.connect(_on_id_pressed)
+	
+	about_window = PopupManager.create("res://scenes/about_dialog.tscn")
+	about_window.name = "AboutDialog"
+	about_window.title = "About"
+	add_child(about_window)
