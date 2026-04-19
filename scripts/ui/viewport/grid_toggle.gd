2a9a9959d21b60b5de8d496065e6b48a4dd97558
Jackie <sharkaccino@fastmail.com>
2026-04-18 17:53:44 -0700

    viewport: add grid toggle button
    Signed-off-by: Jackie <sharkaccino@fastmail.com>

---
+++ scripts/ui/viewport/grid_toggle.gd
@@ -0,0 +1,14 @@
+extends CheckButton
+
+@onready var canvas_grid = %CanvasGrid
+
+func _pressed() -> void:
+	canvas_grid.visible = button_pressed
+
+func check_control_activation() -> void:
+	var project_opened = StateManager.active_project_id != 0
+	disabled = !project_opened
+
+func _ready() -> void:
+	check_control_activation()
+	StateManager.active_project_changed.connect(check_control_activation)
