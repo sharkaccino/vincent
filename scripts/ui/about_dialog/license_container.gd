d0ebea95cbaef073a9bece829cbb803619911eb4
Jackie <sharkaccino@fastmail.com>
2026-04-29 17:32:59 -0700

    theme: update default font, improved "about" dialog
    Signed-off-by: Jackie <sharkaccino@fastmail.com>

--- scripts/ui/about_dialog/license_container.gd
+++ scripts/ui/about_dialog/license_container.gd
@@ -4,9 +4,3 @@ extends PanelContainer
 
 func _ready() -> void:
 	visible = false
-	
-	var reader = FileAccess.open("res://LICENSE", FileAccess.READ)
-	var license_text = reader.get_as_text()
-	reader.close()
-	
-	label.text = license_text
--- scripts/ui/about_dialog/toggle_license.gd
+++ scripts/ui/about_dialog/toggle_license.gd
@@ -1,9 +1,13 @@
-extends CheckButton
+extends Button
 
 var license_container: PanelContainer
 
+var _toggled = false
+
 func _pressed() -> void:
-	license_container.visible = button_pressed
+	_toggled = !_toggled
+	license_container.visible = _toggled
+	text = "Hide License" if _toggled else "Show License"
 
 func _ready() -> void:
 	await %ContentWrapper.ready
